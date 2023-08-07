f42a9d2166816b346caf7947bc66cb5588c1b646
AbelClapton <abelrguezxd@gmail.com>
2023-08-07 18:56:03 -0400

    update main.py

--- src/main.py
+++ src/main.py
@@ -1,14 +1,8 @@
-from fastapi import FastAPI, Depends, HTTPException
-from sqlalchemy.orm import Session
+from fastapi import FastAPI
 
-from .database import SessionLocal, engine
-from . import models
-from .routers import users, pools
-
-models.Base.metadata.create_all(bind=engine)
+from .database import engine
+from .utils import load_routers
 
 app = FastAPI()
 
-
-app.include_router(users.router)
-app.include_router(pools.router)
+load_routers(app)
