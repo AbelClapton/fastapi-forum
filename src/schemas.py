cf71f09e5ff96f7bbe148cf70499e82133c0626e
AbelClapton <abelrguezxd@gmail.com>
2023-08-07 18:56:25 -0400

    add Message to global schemas

--- src/schemas.py
+++ src/schemas.py
@@ -1,39 +1,5 @@
-from __future__ import annotations
-from typing import Annotated
-
-from pydantic import BaseModel, Field, EmailStr
+from pydantic import BaseModel
 
 
-class PoolBase(BaseModel):
-    entry: Annotated[float, Field(min=1)] = 1
-    capacity: Annotated[int, Field(min=2)] = 100
-
-
-class PoolCreate(PoolBase):
-    pass
-
-
-class Pool(PoolBase):
-    id: int
-
-    class Config:
-        orm_mode = True
-
-
-class UserBase(BaseModel):
-    email: EmailStr
-    username: Annotated[str | None, Field(min_length=3)] = None
-    balance: Annotated[float, Field(min=0)] = 0
-
-
-class UserCreate(UserBase):
-    password: Annotated[str, Field(min_length=3)]
-
-
-class User(UserBase):
-    id: int
-    is_active: bool = True
-    pools: list[Pool] = []
-
-    class Config:
-        orm_mode = True
+class Message(BaseModel):
+    message: str
